ef0e3aeb165652e0b7cc5cee0867a3324132f471
Todai88 <joabaj88@gmail.com>
2016-11-01 21:53:49 +0100

    kmom04

---
+++ kmom04/lab3/answer.js
@@ -0,0 +1,555 @@
+/**
+ * @preserve 2381f3f7aa0352ca06e23cb904ee0b04
+ *
+ * 2381f3f7aa0352ca06e23cb904ee0b04
+ * javascript1
+ * lab3
+ * jobk16
+ * 2016-11-01 20:49:42
+ * v2.2.21 (2016-10-07)
+ *
+ * Generated 2016-11-01 21:49:42 by dbwebb lab-utility v2.2.21 (2016-10-07).
+ * https://github.com/mosbth/lab
+ */
+
+(function(dbwebb){
+    "use strict";
+
+    var ANSWER = null;
+
+    console.log("Ready to begin.");
+
+
+/** ======================================================================
+ * Lab 3 - javascript1
+ *
+ * Practise arrays. You might find useful help here:  
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Ob
+ * jects/Array
+ *
+ */
+
+
+
+/** ----------------------------------------------------------------------
+ * Section 1 . Arrays
+ *
+ * To copy an array use array.slice() to make a real copy, not a shallow one.
+ *
+ */
+
+
+
+/**
+ * Exercise 1.1
+ *
+ * Create a variable 'array1' holding an array with the numbers
+ * 41,76,-16,0,-462,699,4,1300.  
+ * 
+ * Answer with the array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.1", ANSWER, false);
+
+/**
+ * Exercise 1.2
+ *
+ * Use the variable 'array1'. How many items does the array have?   
+ * 
+ * Answer with the result.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.2", ANSWER, false);
+
+/**
+ * Exercise 1.3
+ *
+ * Create a variable 'array2' holding an array with the words:
+ * milk,juice,lemonade,soda,water.  
+ * 
+ * Answer with the array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.3", ANSWER, false);
+
+/**
+ * Exercise 1.4
+ *
+ * Return the element on position: 1 in 'array2'.  
+ * 
+ * Answer with the result.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.4", ANSWER, false);
+
+/**
+ * Exercise 1.5
+ *
+ * Return element 1 in 'array2'. Answer with the result.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.5", ANSWER, false);
+
+/**
+ * Exercise 1.6
+ *
+ * Use the variable 'array2'. Concatenate the first item and the last item as
+ * a string. Separate the string with '-'.  
+ * 
+ * Answer with the result.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.6", ANSWER, false);
+
+/**
+ * Exercise 1.7
+ *
+ * Merge the two arrays 'array1' and 'array2' into a third variable 'array3'. 
+ * 
+ * 
+ * Answer with 'array3'.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.7", ANSWER, false);
+
+/**
+ * Exercise 1.8
+ *
+ * Reverse the order of the elements in array 'array3'.  
+ * 
+ * Answer with the resulting array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.8", ANSWER, false);
+
+/**
+ * Exercise 1.9
+ *
+ * Create a variable 'array21' as a clone of 'array2'. Sort 'array21'. Answer
+ * with the resulting array.  
+ * (Hint:
+ * http://stackoverflow.com/questions/3978492/javascript-fastest-way-to-duplic
+ * ate-an-array-slice-vs-for-loop)
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.9", ANSWER, false);
+
+/**
+ * Exercise 1.10
+ *
+ * Create a variable 'array11' as a copy of 'array1'. Sort 'array11' according
+ * to its values. The smallest value comes first and the largest value comes
+ * last.  
+ * 
+ * Answer with the resulting array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.10", ANSWER, false);
+
+/**
+ * Exercise 1.11
+ *
+ * Create a variable 'array22' which holds the same content as 'array2' - but
+ * all strings are uppercase. Use the built-in Array-function 'map()' to solve
+ * it.  
+ * 
+ * Answer with the array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.11", ANSWER, false);
+
+/**
+ * Exercise 1.12
+ *
+ * Create a new array 'messageOfToday'. It should contain all items from
+ * 'array2' where each item is concatenated with the string ' is good for
+ * you!'. Use the built-in array-function 'forEach()' to solve it. Each
+ * sentence shold start with a capital letter.  
+ * 
+ * Answer with the resulting array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.12", ANSWER, false);
+
+/**
+ * Exercise 1.13
+ *
+ * Create a new array 'array12'. It should contain all positive numbers from
+ * the 'array1'. Use the built-in array-function 'filter()' to solve it.  
+ * 
+ * Answer with the resulting array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.13", ANSWER, false);
+
+/**
+ * Exercise 1.14
+ *
+ * Create a variable 'iLike'. It should contain the string 'I like ' and then
+ * all items from 'array2' separated with ', '. End the string with a '!'. Use
+ * the built-in array-function 'join()' to solve it.  
+ * 
+ * Answer with the string.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.14", ANSWER, false);
+
+/**
+ * Exercise 1.15
+ *
+ * Create a function 'arraySum' that takes one array as argument and returns
+ * the sum of all elements in that array.  
+ * 
+ * Try out the function using 'array1' and answer with the result.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.15", ANSWER, false);
+
+/**
+ * Exercise 1.16
+ *
+ * Create a function 'arrayAverage' that takes one array as argument and
+ * returns the average of all elements in that array.  
+ * 
+ * Try out the function using 'array1' and answer with the rounded integer
+ * result.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.16", ANSWER, false);
+
+/** ----------------------------------------------------------------------
+ * Section 2 . Modify arrays
+ *
+ * Learn how to modify arrays.
+ *
+ */
+
+
+
+/**
+ * Exercise 2.1
+ *
+ * Create a new array 'myArray' and make it a copy of 'array1'. Get the last
+ * element from 'myArray' using the built-in array-function 'pop()'.  
+ * 
+ * Answer with the resulting array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.1", ANSWER, false);
+
+/**
+ * Exercise 2.2
+ *
+ * Add the boolean value 'true' to the array 'myArray' using built-in
+ * array-function 'push()'.  
+ * 
+ * Answer with the resulting array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.2", ANSWER, false);
+
+/**
+ * Exercise 2.3
+ *
+ * Use the built-in array-function 'shift()' to remove the first item from the
+ * array 'myArray'.  
+ * 
+ * Answer with the resulting array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.3", ANSWER, false);
+
+/**
+ * Exercise 2.4
+ *
+ * Add the float number '3.14' to the beginning of 'myArray' using built-in
+ * array-function 'unshift()'.  
+ * 
+ * Answer with the resulting array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.4", ANSWER, false);
+
+/**
+ * Exercise 2.5
+ *
+ * Change 'myArray' element 3 and 4 to the boolean value 'false' using
+ * built-in array-function 'splice()'.  
+ * 
+ * Answer with the resulting array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.5", ANSWER, false);
+
+/**
+ * Exercise 2.6
+ *
+ * Extract the last two elements as a slice from 'myArray' using built-in
+ * array-function 'slice()'.  
+ * 
+ * Answer with the slice array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.6", ANSWER, false);
+
+/**
+ * Exercise 2.7
+ *
+ * Remove item 4 and 5 in 'myArray'. Answer with the resulting array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.7", ANSWER, false);
+
+/**
+ * Exercise 2.8
+ *
+ * Insert the string 'MEGA' after item 3 in 'myArray'. Answer with the
+ * resulting array.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.8", ANSWER, false);
+
+
+    console.log(dbwebb.exitWithSummary());
+
+}(window.dbwebb));
---
+++ kmom04/lab4/answer.js
@@ -0,0 +1,297 @@
+/**
+ * @preserve 7a11ac5b7d8538ff1943b618d5cd5a63
+ *
+ * 7a11ac5b7d8538ff1943b618d5cd5a63
+ * javascript1
+ * lab4
+ * jobk16
+ * 2016-11-01 20:49:54
+ * v2.2.21 (2016-10-07)
+ *
+ * Generated 2016-11-01 21:49:54 by dbwebb lab-utility v2.2.21 (2016-10-07).
+ * https://github.com/mosbth/lab
+ */
+
+(function(dbwebb){
+    "use strict";
+
+    var ANSWER = null;
+
+    console.log("Ready to begin.");
+
+
+/** ======================================================================
+ * Lab 4 - javascript1
+ *
+ * Practice basics on objects.
+ *
+ */
+
+
+
+/** ----------------------------------------------------------------------
+ * Section 1 . Create object
+ *
+ * Start by creating an empty object called 'person' by using the object
+ * literal.
+ *
+ */
+
+
+
+/**
+ * Exercise 1.1
+ *
+ * Give your person-object the property 'firstName' with a value of 'Henri'.
+ * Add a method called 'print1()' that returns a presentation of the object,
+ * like this: 'My name is Henri.' Use 'this.firstName' to construct the
+ * string.  
+ * 
+ * Answer with the result from calling 'person.print1()'.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.1", ANSWER, false);
+
+/**
+ * Exercise 1.2
+ *
+ * Add properties 'lastName' and 'nationality' to your person-object. Their
+ * values should be 'Becquerel' and 'France'.  
+ * 
+ * Create the method 'person.print2()' which should say: 'My name is Henri
+ * Becquerel from France.'.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.2", ANSWER, false);
+
+/**
+ * Exercise 1.3
+ *
+ * Add the property 'born' with the value of a Date object: '1852-12-15'.
+ * Create a method 'print3()' that says exactly the same as 'print2()'
+ * followed by 'I was born 1852.'.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.3", ANSWER, false);
+
+/**
+ * Exercise 1.4
+ *
+ * Create a second person, called 'person2' by using built-in function
+ * 'Object.create()'. The person2 should have the following properties:
+ * 'Albert, Einstein, Germany, 1879-03-14'.  
+ * 
+ * Print out details on the second person using 'person2.print3()'.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("1.4", ANSWER, false);
+
+/** ----------------------------------------------------------------------
+ * Section 2 . More on objects
+ *
+ * Even more on objects!
+ *
+ */
+
+
+
+/**
+ * Exercise 2.1
+ *
+ * Create a object called 'shape' with the properties: 'x', 'y', 'height',
+ * 'width' and 'print'. Create a new object from 'shape' called 'shape1' and
+ * initiate the properties with: x:29, y:56, height:17, width: 19.  
+ * 
+ * Use the 'print' method to print out the assigned values as: 'x:?, y:?,
+ * height:?, width:?'
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.1", ANSWER, false);
+
+/**
+ * Exercise 2.2
+ *
+ * Create a method 'shape.init(x, y, height, width)' that helps you to
+ * initiate a object. Try it out by re-initing 'shape1' using the method.  
+ * 
+ * Answer with 'shape1.print()'.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.2", ANSWER, false);
+
+/**
+ * Exercise 2.3
+ *
+ * Create another object from 'shape' called 'shape2' and initiate the
+ * properties with: x:64, y:27, height:12, width: 12.  
+ * 
+ * Use the 'print' method to print out the assigned values.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.3", ANSWER, false);
+
+/**
+ * Exercise 2.4
+ *
+ * Create a method in 'shape' that calculates and returns the area of the
+ * shape. Try it out by calling it for 'shape1' and 'shape2'.  
+ * 
+ * Answer with both values, separated by ', ' (notice the space after the
+ * comma).
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.4", ANSWER, false);
+
+/**
+ * Exercise 2.5
+ *
+ * Create a method 'shape.overlapPoint(posX, posY)' that checks if a position
+ * x, y is within the current shape. Or, the shape overlaps that position. The
+ * shapes position x and y is top left of the shape. Return true or false.
+ * Test by checking if x:47, y:72 is within 'shape1' and if x:63, y:26 is
+ * within 'shape2'.  
+ * 
+ * Return the result separated by ', ' (notice the space after the comma).
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.5", ANSWER, false);
+
+/**
+ * Exercise 2.6
+ *
+ * Create a method 'shape.overlapShape()' that takes a shape as argument and
+ * checks if the shapes overlaps (colliding bodies). Return true or false.
+ * Create a new 'shape3' and initiate the properties with: x:39, y:65,
+ * height:17, width: 19.  
+ * 
+ * Return the result from checking 'shape1.overlapShape(shape3)'.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.6", ANSWER, false);
+
+/**
+ * Exercise 2.7
+ *
+ * Create a method 'shape.move(moveX, moveY)' which moves the shape from its
+ * current position by adding 'x += moveX' and 'y += moveY'. Try it out by
+ * moving 'shape3' using 'moveX: 19, moveY: 17'.  
+ * 
+ * Re-check if the bodies 'shape1' and 'shape3' collides.
+ *
+ * Write your code below and put the answer into the variable ANSWER.
+ */
+
+
+
+
+
+
+ANSWER = "Replace this text with the variable holding the answer.";
+
+// I will now test your answer - change false to true to get a hint.
+dbwebb.assert("2.7", ANSWER, false);
+
+
+    console.log(dbwebb.exitWithSummary());
+
+}(window.dbwebb));
