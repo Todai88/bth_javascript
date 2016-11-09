f5b4994ff62737e4a82b0aa8bc3d3a414d87b884
Joakim Kakaei <joabaj88@gmail.com>
2016-11-09 15:58:42 +0000

    adding flag3

---
+++ kmom04/flag3/js/main.js
@@ -0,0 +1,60 @@
+(function(){
+    'use strict';
+
+    //var myContent = document.getElementById('content');
+
+    //myContent.innerHTML = '<h3>This is a template!</h3>';
+
+    console.log('Sandbox is ready!');
+
+})();
+
+function click_parse(e){
+    console.log('I am ' + e.target.id);
+    draw(e.target.id);
+}
+
+function draw(sender){
+    var Flag = {
+        flag_name : {value: ''},
+        body : {value: ''},
+        init : function(flag, content){
+            return Object.create(Flag, {flag_name: {value : flag},
+                                        body : {value : content}
+                                    });
+        },
+        draw : function(){
+            this.flag_name.innerHTML = this.body;
+            this.flag_name.addEventListener("click", function () {
+                this.innerHTML = '';
+            });
+        }
+    };
+    var target;
+    switch(sender){
+        case 'Swe_click':
+            var sweden = Flag.init(document.getElementById('flag_container_swe'), '<div class="flag" id="sverige"></div>');
+            sweden.draw();
+            break;
+        case 'Jpn_click':
+            var japan = Flag.init(document.getElementById('flag_container_japan'), '<div class="flag" id="japan"> <div id="circle"> </div></div>');
+            japan.draw();
+            break;
+        case 'Chl_click':
+            var chile = Flag.init(document.getElementById('flag_container_chile'),
+                                                          '<div class="flag" id="chile">' +
+                                                          '<div class="flex-row-container"> <div class="flex-row-item" id="top-row-item">' +
+                                                          '<div class="flex-col-container"> <div class="flex-col-item" id="left-col-item">' +
+                                                          '<div id="star-five">  </div> </div> <div class="flex-col-item" id="right-col-item">' +
+                                                          '</div> </div> </div> <div class="flex-row-item" id="btm-row-item"> </div> </div> </div>');
+            chile.draw();
+            break;
+        case 'Col_click':
+            var colombia = Flag.init(document.getElementById('flag_container_colombia'),
+                                     '<div id="colombia"><div class="flag"> <div class="flex-row-container">'+
+                                     '<div class="flex-row-item" id="colombia1"> </div> <div class="flex-row-item" id="colombia2"> </div>' +
+                                     '<div class="flex-row-item" id="colombia3"> </div> </div> </div></div>')
+            colombia.draw();
+            break;
+     }
+} 
